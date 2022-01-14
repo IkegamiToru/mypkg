1d15f360f425e2f324119bebaff8e4440f21e508
Toru Ikegami <s20C1007NU@s.chibakoudai.jp>
2022-01-14 14:03:50 +0000

    Update-scripts

--- scripts/count.py
+++ scripts/count.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#author:Ryuichi Ueda
+#LICENSE:BSD
 
 import rospy
 from std_msgs.msg import Int32
--- scripts/twice.py
+++ scripts/twice.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#Author:Ryuichi Ueda
+#LICENSE BSD
 
 import rospy
 from std_msgs.msg import Int32
